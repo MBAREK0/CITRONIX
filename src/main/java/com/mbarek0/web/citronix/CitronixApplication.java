986d017071fb3f90657be004369c154ab62d38e0
MBAREK0 <elaadraouimbarek2023@gmail.com>
2024-11-17 19:16:12 +0100

    Set up the application properties (database connection).

---
+++ src/main/java/com/mbarek0/web/citronix/CitronixApplication.java
@@ -0,0 +1,13 @@
+package com.mbarek0.web.citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitronixApplication.class, args);
+    }
+
+}
